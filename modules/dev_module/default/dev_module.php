2680445a9420d7dfbcd874b038e8b3278c3173cf
Kenneth Sharp <ksharp@stanford.edu>
2016-11-07 17:02:53 -0800

    Updated with Hartley extra fields.

---
+++ modules/dev_module/default/dev_module.php
@@ -0,0 +1,22 @@
+<?php
+    if (!empty($_REQUEST['base_path']) && !empty($_REQUEST['cookie_domain']) &&
+        !empty($_REQUEST['destination'])) {
+        $tmp_dir = substr($_SERVER['SCRIPT_FILENAME'],0,strpos($_SERVER['SCRIPT_FILENAME'],'user0_webauth.php')) . 'tmp';
+        $tempnam = tempnam($tmp_dir,'');
+        $ldap_vars = array();
+        foreach ($_SERVER as $key => $value) {
+            if (substr_compare($key, 'WEBAUTH_LDAP_',0,13) == 0) {
+                $ldap_vars[$key] = $value;
+            }
+        }
+        $ldap_vars['WEBAUTH_LDAP_DESTINATION'] = $_REQUEST['destination'];
+        if (file_put_contents($tempnam,json_encode($ldap_vars)) !== false) {
+            $fname = substr($tempnam,strrpos($tempnam,'/')+1);
+            setcookie('user0_webauth',$fname,0,$_REQUEST['base_path'],$_REQUEST['cookie_domain'],TRUE,TRUE);
+        }
+    	header('Location: http://'.$_SERVER['HTTP_HOST'].':'.$_SERVER['HTTP_PORT'].$_REQUEST['base_path'].$_REQUEST['destination']);
+    } else  {
+	header('HTTP/1.0 404 Not Found');
+        header('Location: http://'.$_SERVER['HTTP_HOST']);
+    }
+?>
