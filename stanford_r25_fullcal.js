02f46d75630cdff3f1e8e732a4af697376518bb0
Kenneth Sharp <ksharp@stanford.edu>
2017-01-11 17:09:35 -0800

    Added more comments to code.

--- stanford_r25_fullcal.js
+++ stanford_r25_fullcal.js
@@ -6,7 +6,7 @@ var qtip = false;  // assume we don't have the qtip library to start
 
     $(document).ready(function () {
 
-	// try to prevent double form-submits when user double-clicks
+	    // try to prevent double form-submits when user double-clicks
         $("#stanford-r25-reservation").submit(function(e){
             $('.form-submit').attr('disabled','disabled');
         });
@@ -14,22 +14,28 @@ var qtip = false;  // assume we don't have the qtip library to start
         // set the date field in the reserve form as the initial focus for the form
         $('#edit-stanford-r25-booking-date-datepicker-popup-0').focus();
 
-        // if we are coming back from a reservation, check cookies for date and calendar view to bring the user back to
+        // if we are coming back from a reservation, check cookies for date to bring the user back to
         var defaultDate = readCookie("stanford-r25-date");
         if (defaultDate === null) {
+            // if no cookie, see if a date was set by Drupal from a URL parameter
             if (Drupal.settings.hasOwnProperty("stanfordR25ParamDate")) {
                 defaultDate = Drupal.settings.stanfordR25ParamDate;
             } else {
+                // otherwise, just use today's date
                 defaultDate = new Date();
             }
         }
+        // cookie would be a single-use thing, so delete it
         deleteCookie("stanford-r25-date");
 
+        // if we are coming back from  a reservation, check cookies for the calendar view
         var defaultView = readCookie("stanford-r25-view");
         if (defaultView === null) {
+            // if no cookie, see if a view was set by Drupal from a URL parameter
             if (Drupal.settings.hasOwnProperty("stanfordR25ParamView")) {
                 defaultView = Drupal.settings.stanfordR25ParamView;
             } else {
+                // otherwise, use the Default view set by Drupal for this room
                 switch (Drupal.settings.stanfordR25DefaultView) {
                     case 1:
                         defaultView = 'agendaDay';
@@ -41,32 +47,40 @@ var qtip = false;  // assume we don't have the qtip library to start
                         defaultView = 'month';
                         break;
                     default:
+                        // finally, default to month view if no other choice
                         defaultView = 'month';
                 }
             }
         }
+        // delete single-use cookie from reservation
         deleteCookie("stanford-r25-view");
 
         // get the romm id set on the server in Drupal
         var stanford_r25_room = Drupal.settings.stanfordR25Room;
 
-        // allow the use of qtip if available and the user's permissions and the room's settings are appropos.
+        // allow the use of qtip tooltips if available and the user's permissions and the room's settings are appropos.
         if (Drupal.settings.stanfordR25Qtip == 'qtip' && Drupal.settings.stanfordR25Access == 1 && Drupal.settings.stanfordR25Status > 0) {
             qtip = true;
         }
+        
+        // get the room status to see if it is enabled
         var stanford_r25_status = Drupal.settings.stanfordR25Status;
 
         // the calendar is selectable by the user if the room is bookable and the user has access
-        var multiDay = false;
-        var selectConstraint = {start: '06:00', end: '22:00'};
-        var selectable = false;
+        var multiDay = false;  // typically do not allow multi-day reservation
+        var selectConstraint = {start: '06:00', end: '22:00'};  // limit selection to "normal" hours
+        var selectable = false;  // value of selectable will determine if user can select timeslots from fullcalendar
         if (parseInt(stanford_r25_status) > 1 && parseInt(Drupal.settings.stanfordR25Access) == 1) {
+            // in this case, the room is reservable and the user has access to reserve it
             selectable = true;
             if (parseInt(Drupal.settings.stanfordR25MultiDay) == 1) {
+                // for multi-day rooms, remove the hour constraint
                 multiDay = true;
                 selectConstraint = {};
             }
         }
+        
+        // some rooms constrain how far into the future a user can reserve. 
         var calendarLimit = new Date(parseInt(Drupal.settings.stanfordR25CalendarLimitYear),
             parseInt(Drupal.settings.stanfordR25CalendarLimitMonth));
 
@@ -87,6 +101,8 @@ var qtip = false;  // assume we don't have the qtip library to start
             document.cookie = "stanford-r25-date=" + $('#edit-stanford-r25-booking-date-datepicker-popup-0').val();
             return true;
         });
+        
+        // configure the fullcalendar plugin
         $('#calendar').fullCalendar({
             allDaySlot: false,
             // if in month view for a non-multi-day room and the user clicks a date, go to agenda day view
@@ -122,13 +138,14 @@ var qtip = false;  // assume we don't have the qtip library to start
                 }
             },
             viewRender: function(view, element) {
+                // when rendering the calendar, add a permalink to the date and view if room config added permalink id
                 if ($('#permalink').length) {
                     var permalink = location.origin + location.pathname +
                         '?view=' + view.name + '&date=' +
                         $('#calendar').fullCalendar('getDate').format('YYYY-MM-DD');
                     $('#permalink').html('<a href="' + permalink + '">Permalink to this page</a>');
-                    //console.log('permalink: ' + permalink);
                 }
+                // if there is an upper limit on calendar view, hide (or show) the 'Next' button
                 if ( calendarLimit < view.end) {
                     $("#calendar .fc-next-button").hide();
                     return false;
@@ -168,6 +185,7 @@ var qtip = false;  // assume we don't have the qtip library to start
             select: function (start, end) {
                 $('#edit-stanford-r25-booking-date-datepicker-popup-0').val(start.format('YYYY-MM-DD'));
                 $('#edit-stanford-r25-booking-date-timeEntry-popup-1').val(start.format('hh:mm a'));
+                // account for multi-day rooms that have an end date/time instead of a duration
                 if (multiDay) {
                     $('#edit-stanford-r25-booking-enddate-datepicker-popup-0').val(end.format('YYYY-MM-DD'));
                     $('#edit-stanford-r25-booking-enddate-timeEntry-popup-1').val(end.format('hh:mm a'));
